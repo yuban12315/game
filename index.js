ad115063abbc3bee4ba1db8c8dccfd5ef0070267
yuban12315 <fengyulin@imudges.com>
2017-07-24 14:38:10 +0800

    build

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict"
+let express=require('express')
+let app=express()
+let server=require('http').Server(app)
+let WebSocketServer = require('ws').Server
+let wss=new WebSocketServer({server})
